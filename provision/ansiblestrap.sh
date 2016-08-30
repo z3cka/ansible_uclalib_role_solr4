50eaecfaf95528856c4b5b339fc1d9e9438ac430
Anthony Vuong <avuong@library.ucla.edu>
2016-08-30 12:28:48 -0700

    LDSE user accounts added by default

--- provision/ansiblestrap.sh
+++ provision/ansiblestrap.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-if [ -z $HOST ];
+if [ -z $INVENTORY ];
 then
-echo "HOST is undefined. Please set the variable"
+  echo "INVENTORY is undefined. Please set the variable"
 exit
 fi
 
 if [ -z $LIMIT ];
 then
-echo "LIMIT is undefined. Please set the variable"
+  echo "LIMIT is undefined. Please set the variable"
 exit
+fi
+
+if [ $INVENTORY = "vagrant.ini" ];
+then
+  ANSIBLE_ENV="vagrant"
+elif [ $INVENTORY = "aws.ini" ];
+then
+  ANSIBLE_ENV="aws"
+else
+  ANSIBLE_ENV="vmware"
 fi
 
 ANSIBLEROOT="/etc/ansible"
 VAULTPASSWORD="/home/ansible/ansible_key"
 
-PLAYBOOKCMD="ansible-playbook -i $HOST -l $LIMIT"
+cd $ANSIBLEROOT
+
+PLAYBOOKCMD="ansible-playbook -i $ANSIBLEROOT/$INVENTORY -l $LIMIT --output=dump -e "ansible_env=$ANSIBLE_ENV""
 
 # Create LDSE accounts
-echo "$PLAYBOOKCMD plays/uclalib_ldse_users.yml -v --vault-password-file $VAULTPASSWORD"
+$PLAYBOOKCMD $ANSIBLEROOT/plays/uclalib_ldse_users.yml -v --vault-password-file $VAULTPASSWORD
