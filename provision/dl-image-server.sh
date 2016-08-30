ceb397e10b097a3ca723661d3963a42360a9a099
Anthony Vuong <avuong@library.ucla.edu>
2016-08-30 12:29:16 -0700

    Add DLP accounts and provision jiiify framework

--- provision/dl-image-server.sh
+++ provision/dl-image-server.sh
@@ -9,14 +9,14 @@
 ###########################################################
 
 # Defined playbook variables
-HOST="$1"
+INVENTORY="$1"
 LIMIT="$2"
 
 # Set ansible-playbook strap
 source ansiblestrap.sh
 
 # Create DLP users
-echo "$PLAYBOOKCMD plays/uclalib_dlp_users.yml -v --vault-password-file $VAULTPASSWORD"
+$PLAYBOOKCMD $ANSIBLEROOT/plays/uclalib_dlp_users.yml -v --vault-password-file $VAULTPASSWORD
 
 # Provision Jiiify framework
-echo "$PLAYBOOKCMD plays/iiifimage.yml -v --vault-password-file $VAULTPASSWORD"
+$PLAYBOOKCMD $ANSIBLEROOT/plays/iiif_images.yml -v --vault-password-file $VAULTPASSWORD
